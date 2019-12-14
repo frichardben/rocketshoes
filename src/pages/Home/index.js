ede0a50570eaed6cbf9719fb5126142f8e17aa6a
Richard Ben <frichardben@yahoo.com.br>
2019-12-14 15:18:51 -0300

    home page stylization

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+ import { Productlist } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <Productlist>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-vr-sneaker-meia-leve/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-vr-sneaker-meia-leve/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-vr-sneaker-meia-leve/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-vr-sneaker-meia-leve/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-vr-sneaker-meia-leve/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-vr-sneaker-meia-leve/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+    </Productlist>
   );
 }
