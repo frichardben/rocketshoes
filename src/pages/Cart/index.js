37d2497ef5f62c099eb08f0918200f80ee9889b5
Richard Ben <frichardben@yahoo.com.br>
2019-12-15 14:30:22 -0300

    refactoring actions

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
 import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+
+import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
- function Cart({ cart, dispatch }) {
+ function Cart({ cart, removeFromCart }) {
   return (
     <Container>
       <ProductTable>
@@ -44,9 +47,7 @@ import { Container, ProductTable, Total } from './styles';
               <td>
                 <button
                   type="button"
-                  onClick={() =>
-                  dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
-                  }
+                  onClick={() => removeFromCart(product.id) }
                 >
                   <MdDelete size={20} color="#7159c1"/>
                 </button>
@@ -71,4 +72,7 @@ const mapStateToProps = state => ({
   cart: state.cart,
 });
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
